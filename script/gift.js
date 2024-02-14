6b585c987cca79dcb873bf39884b3ef5acda8328
ehoang0106 <khoahoang1698@gmail.com>
2024-02-13 21:04:15 -0800

    update

--- script/gift.js
+++ script/gift.js
@@ -1,9 +1,46 @@
 $(document).ready(function() {
+    let universalClickCount = 0;
+    let seaWorldClickCount = 0;
+
     $('.gift').click(function() {
-        $('#result').css('display', 'block');
+        let giftType = $(this).data('gift');
+        if (giftType === 'Universal') {
+            universalClickCount++;
+            if (universalClickCount === 2) {
+                $(this).hide();
+            }
+        } else if (giftType === 'SeaWorld') {
+            seaWorldClickCount++;
+            if (seaWorldClickCount === 2) {
+                $(this).hide();
+            }
+        } else {
+            
+            $('#result').css('display', 'block');
+        }
+    });
+
+    $('#submitDate').click(function() {
+        var chosenDate = $('#chosenDate').val();
+        if (chosenDate) {
+         
+            alert(`On ${chosenDate}, we are going to Disneyland!`);
+            
+            $('#dateSelection').css('display', 'none');
+            $('#chosenDate').val(''); 
+            
+            $('#result').css('display', 'none');
+        }
     });
 
     $('.close').click(function() {
         $('#result').css('display', 'none');
+       
+        $('#dateSelection').css('display', 'none');
+    });
+
+    
+    $('.gift[data-gift="Disneyland"]').click(function() {
+        $('#dateSelection').css('display', 'block');
     });
 });
